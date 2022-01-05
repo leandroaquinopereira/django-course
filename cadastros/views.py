92a97d4dcb1fcf6ae39b13915f346b019a9ee3fc
leandroaquinopereira <leandroaquinopereira@outlook.com>
2022-01-05 15:55:09 -0300

    List and Detail

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from cadastros.models import Cidade
@@ -7,4 +7,29 @@ from cadastros.models import Cidade
 def lista_cidades(request):
 
     qs = Cidade.objects.all()
-    qs_capital = Cidade.objects.filter(capital=True)
+
+    context = {
+
+        'cidades': qs,
+        'titulo': 'Inatel'
+
+    }
+
+    return render(request, 'cadastros/lista_cidades.html', context)
+
+def detalhe_cidade(request, id ):
+
+    ## id_cidade = request.GET['id_cidade']
+
+    ## cidade = Cidade.objects.get(pk=id_cidade)
+
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    context = {
+
+        'cidade': cidade
+
+    }
+
+    return render(request, 'cadastros/detalhe_cidades.html', context)
+
