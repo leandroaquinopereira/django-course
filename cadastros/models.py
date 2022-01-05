793b9d044fb2cab92de8fd05967a4b3883b8741a
leandroaquinopereira <leandroaquinopereira@outlook.com>
2022-01-05 00:28:46 -0300

    Create View and New Migrate

--- cadastros/models.py
+++ cadastros/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Cidade(models.Model):
 
     nome = models.CharField(max_length=100)
+    capital = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from cadastros.models import Cidade
+
+
+def lista_cidades(request):
+
+    qs = Cidade.objects.all()
+    qs_capital = Cidade.objects.filter(capital=True)
